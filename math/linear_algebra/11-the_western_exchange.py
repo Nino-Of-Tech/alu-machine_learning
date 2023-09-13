c2c00b7e77483302840c1a20d30eb1e0edb545b5
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-14 02:51:31 +0300

    added functions to files

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for transposing a numpy.ndarray
+"""
+
+
+def np_transpose(matrix):
+    """
+    Transposes a matrix.
+    """
+    import numpy as np
+    return np.transpose(matrix)
