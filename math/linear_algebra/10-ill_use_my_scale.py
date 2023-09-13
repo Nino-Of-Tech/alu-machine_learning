58c22880b788225d6626028e1ba36b26318900eb
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-14 01:51:26 +0300

    added functions to file

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Module for calculating the shape of a numpy.ndarray
+"""
+
+
+import numpy as np
+""" Importing numpy """
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy.ndarray.
+    """
+    return matrix.shape
+
+
+if __name__ == "__main__":
+    mat1 = np.array([1, 2, 3, 4, 5, 6])
+    mat2 = np.array([])
+    mat3 = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]],
+                     [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]])
+    print(np_shape(mat1))
+    print(np_shape(mat2))
+    print(np_shape(mat3))
