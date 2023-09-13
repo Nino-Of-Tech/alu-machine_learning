44dc0398cc5e4750e3b99892e63567716d437dc6
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-14 01:47:33 +0300

    made edits to file

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -10,7 +10,7 @@ def mat_mul(mat1, mat2):
     """
     if len(mat1[0]) != len(mat2):
         return None
-    
+
     result = [
         [
             sum(a * b for a, b in zip(row, col))
@@ -18,7 +18,7 @@ def mat_mul(mat1, mat2):
         ]
         for row in mat1
     ]
-    
+
     return result
 
 
