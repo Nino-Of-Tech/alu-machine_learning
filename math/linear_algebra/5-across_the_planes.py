1c0685894e2672cb56fe8c84009991fdf47f0df0
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-14 00:42:13 +0300

    added functions to file

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+The function add_matrices2D that adds two 2D matrices element-wise.
+"""
+
+
+def add_matrices2D(mat1, mat2):
+    """
+    Adds two 2D matrices element-wise and returns a new matrix.
+    """
+    if len(mat1) != len(mat2) or any(len(row1) != len(row2) for row1, row2 in zip(mat1, mat2)):
+        return None
+
+    return [[a + b for a, b in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+
+
+if __name__ == "__main__":
+    mat1 = [[1, 2], [3, 4]]
+    mat2 = [[5, 6], [7, 8]]
+    print(add_matrices2D(mat1, mat2))
+    print(mat1)
+    print(mat2)
+    print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
