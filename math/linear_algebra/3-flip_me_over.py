5160c4f75aec2fd1187e968703a0e72a29a56645
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-14 00:26:22 +0300

    new file

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+The function matrix_transpose that returns the transpose of a given matrix.
+"""
+
+
+def matrix_transpose(matrix):
+    """
+    Returns the transpose of a 2D matrix.
+    """
+
+    return [list(row) for row in zip(*matrix)]
+
+if __name__ == "__main__":
+    mat1 = [[1, 2], [3, 4]]
+    print(mat1)
+    print(matrix_transpose(mat1))
+    
+    mat2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
+            [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
+    print(mat2)
+    print(matrix_transpose(mat2))
