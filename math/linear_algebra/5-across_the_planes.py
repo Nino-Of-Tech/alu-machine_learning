b83b329dfa4f35052fa3fca87b7e993a23d56d0c
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-14 00:50:33 +0300

    fixed pycodestyle issues

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -9,7 +9,7 @@ def add_matrices2D(mat1, mat2):
     Adds two 2D matrices element-wise and returns a new matrix.
     """
     if len(mat1) != len(mat2) or any(len(row1) != len(row2) for row1,
-            row2 in zip(mat1, mat2)):
+                                                  row2 in zip(mat1, mat2)):
         return None
 
     return [[a + b for a, b in zip(row1, row2)] for row1, row2 in zip
