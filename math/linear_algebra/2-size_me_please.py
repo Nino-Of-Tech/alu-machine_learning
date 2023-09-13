20671bd5e7e54d470272b82eb0cf97e04a7b6324
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-14 00:04:36 +0300

    updated file

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+
+''' Calculates the shape of a matrix '''
+
 def matrix_shape(matrix):
+    ''' Check for matrix and return the shape '''
     shape = []
     while type(matrix) == list:
         shape.append(len(matrix))
