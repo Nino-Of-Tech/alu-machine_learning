0e5304dee2a106b9a1f0076d4044ae77e465c92b
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-14 00:29:28 +0300

    new file

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -11,11 +11,12 @@ def matrix_transpose(matrix):
 
     return [list(row) for row in zip(*matrix)]
 
+
 if __name__ == "__main__":
     mat1 = [[1, 2], [3, 4]]
     print(mat1)
     print(matrix_transpose(mat1))
-    
+
     mat2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15],
             [16, 17, 18, 19, 20], [21, 22, 23, 24, 25], [26, 27, 28, 29, 30]]
     print(mat2)
