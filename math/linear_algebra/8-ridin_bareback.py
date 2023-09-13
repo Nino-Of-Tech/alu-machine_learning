fe75ae9f57d976e7bbb3f33a13c31b9554e3083c
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-14 01:34:00 +0300

    added functions to file

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
-import numpy as np
-matrix = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12],
-                   [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24]])
-mat1 = matrix[1:3, :]  # Gets the middle two rows
-mat2 = matrix[:, 2:4]  # Gets the middle two columns
-mat3 = matrix[-3:-1, -3:]  # Gets the bottom-right 3x3 matrix
-print("The middle two rows of the matrix are:\n{}".format(mat1))
-print("The middle two columns of the matrix are:\n{}".format(mat2))
-print("The bottom-right, square, 3x3 matrix is:\n{}".format(mat3))
+"""
+Module for performing matrix multiplication
+"""
+
+
+def mat_mul(mat1, mat2):
+    """
+    Perform matrix multiplication of two 2D matrices.
+    """
+    if len(mat1[0]) != len(mat2):
+        return None
+    
+    result = [
+        [
+            sum(a * b for a, b in zip(row, col))
+            for col in zip(*mat2)
+        ]
+        for row in mat1
+    ]
+    
+    return result
+
+
+if __name__ == "__main__":
+    mat1 = [[1, 2], [3, 4], [5, 6]]
+    mat2 = [[1, 2, 3, 4], [5, 6, 7, 8]]
+    print(mat_mul(mat1, mat2)))
