149a0e66c4237bb8439cdd4b242f6b9ef4cbb2a3
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-14 03:31:51 +0300

    added new edits

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -2,13 +2,14 @@
 """
 Module to perform slicing on a numpy ndarray along specific axes.
 """
-import numpy as np
 
 
 def np_slice(matrix, axes={}):
     """
     Slices a numpy ndarray along specific axes.
     """
+    import numpy as np
+
     slices = [slice(None)] * matrix.ndim
 
     for axis, slice_tuple in axes.items():
