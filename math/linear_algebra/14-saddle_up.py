a800a2980e611379be657d3befd8d9c85cd4f883
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-14 03:25:30 +0300

    added functions to files

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for performing matrix multiplication.
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication of two numpy.ndarrays.
+    """
+    return np.matmul(mat1, mat2)
