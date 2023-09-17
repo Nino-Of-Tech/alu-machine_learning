17ab5ba23116d1e99a72f104c5ffbb6f8ff794cd
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-17 20:43:50 +0300

    added functions

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -4,11 +4,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 y = np.arange(0, 11) ** 3
+
+# your code here
+
 x = np.arange(0, 11)
-
 plt.plot(x, y, color='red', linestyle='-')
-
 plt.xlim((0, 10))
+
 plt.show()
