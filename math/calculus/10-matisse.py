1c8906d1026fbdc7ed05f34d69d248eace740960
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-26 01:59:11 +0300

    fixed pycodestyle issues

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -5,8 +5,6 @@
 def poly_derivative(poly):
     """derivative of the polynomial
     """
-
-
     if not poly or type(poly) is not list or not all
     (isinstance(i, (int, float)) for i in poly):
         return None  # check if poly is a valid list of numbers
