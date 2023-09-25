5706ec26f64500d6f8d7141d0890578c01231736
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-09-26 01:27:07 +0300

    added new functions

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""coefficient of the derivative of the polynomial"""
+
+
+def poly_derivative(poly):
+    if not poly or type(poly) is not list or not all(isinstance(i,
+        (int, float)) for i in poly):
+        return None  # check if poly is a valid list of numbers
+
+    derivative = [poly[i] * i for i in range(1, len(poly))] 
+    # apply the power rule for each term
+
+    while derivative and derivative[-1] == 0:
+        derivative.pop()
+
+    return derivative if derivative else [0] 
+    # return [0] if derivative is empty, else return derivative
+
+poly = [5, 3, 0, 1]
+print(poly_derivative(poly))
