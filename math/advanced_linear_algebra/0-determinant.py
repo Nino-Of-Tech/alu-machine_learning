ebe060bcff50a53d516f8d814372a2892c4a0762
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-10-15 15:46:20 +0300

    added function for find ing a determinant

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -2,17 +2,24 @@
 def determinant(matrix):
     """
     Calculates the determinant of a matrix
+
     Args:
-    - matrix   : list of list
-                 matrix of which the determinant should be calculated
+        matrix (list of lists): Matrix of which the determinant should be calculated.
+
+    Returns:
+        float: Determinant of the matrix.
+
+    Raises:
+        TypeError: If matrix is not a list of lists.
+        ValueError: If matrix is not a square matrix.
 
     """
 
     # Check if matrix is a list of lists
-    if not isinstance(matrix, list) or not all(isinstance(row, list) 
-            for row in matrix):
+    if not isinstance(matrix, list) or not
+    all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
-    
+
     # Check for an empty matrix [[]]
     if len(matrix) == 1 and len(matrix[0]) == 0:
         return 1
@@ -20,7 +27,6 @@ def determinant(matrix):
     # Check for a non-square matrix
     if len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a square matrix")
-    
     # Base case: 1x1 matrix
     if len(matrix) == 1 and len(matrix[0]) == 1:
         return matrix[0][0]
@@ -36,6 +42,5 @@ def determinant(matrix):
         cofactor = ((-1) ** j) * matrix[0][j] * determinant(
                 [row[:j] + row[j+1:] for row in matrix[1:]])
         det += cofactor
-    
-    return det
 
+    return det
