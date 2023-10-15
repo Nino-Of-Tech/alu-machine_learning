3f89bd3fd62894d08bef48f4a8f3e549c1eedcbf
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-10-15 17:26:16 +0300

    added functions to calculate the minor of a matrix

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -1,47 +1,15 @@
 #!/usr/bin/env python3
 
-""" Module for calculating the minor of a matrix from a determinant """
+""" Module to calculate the minor of a matrix from determinant """
 
-
-def determinant(matrix):
-    """
-    Calculates the determinant of a matrix
-    """
-    # Check if matrix is a list of lists
-    if not isinstance(matrix, list) or
-    not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a list of lists")
-
-    # Check for an empty matrix [[]]
-    if len(matrix) == 1 and len(matrix[0]) == 0:
-        return 1
-
-    # Check for a non-square matrix
-    if any(len(row) != len(matrix) for row in matrix):
-        raise ValueError("matrix must be a square matrix")
-
-    # Base case: 1x1 matrix
-    if len(matrix) == 1:
-        return matrix[0][0]
-
-    # Base case: 2x2 matrix
-    if len(matrix) == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-
-    # Recursive case for a matrix of size 3x3 or larger
-    det = 0
-    for j in range(len(matrix)):
-        cofactor = ((-1) ** j) * matrix[0][j] * determinant(
-                [row[:j] + row[j+1:] for row in matrix[1:]])
-        det += cofactor
-
-    return det
+determinant = __import__('0-determinant').determinant
 
 
 def minor(matrix):
     """
     Calculates the minor matrix of a matrix
     """
+
     # Check if matrix is a list of lists
     if not isinstance(matrix, list) or not all(isinstance(
      row, list) for row in matrix):
