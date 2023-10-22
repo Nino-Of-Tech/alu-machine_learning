794ac305ddbe09cb7bf3841af05f5acc5e249b41
Nino_Of_Tech <nwachukwunino15@gmail.com>
2023-10-23 00:36:00 +0300

    added functions to calculate mean and covariance

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -3,37 +3,20 @@ import numpy as np
 
 """ A function that calculates the mean and covariance of a data set """
 
-""" X is a numpy.ndarray of shape (n, d) containing the data set"""
+""" X is a numpy.ndarray of shape (n, d) containing the data set """
 
-
-def mean_cov(X):
-    X = np.array(X)
-    """ n is the number of data points """
-    """ d is the number of dimensions in each data point """
-
-    (n, d) = X.shape
-
+def mean_cov(x):
     """
-    If X is not a 2D numpy.ndarray, raise a TypeError
-    with the message X must be a 2D numpy.ndarray
+    calculate mean and covariance of a data set
+    x - numpy.ndarray - data set, shape (n, d)
+    n - int - number of data points
+    d - int - number of dimensions
     """
-
-    """ If X.ndim !=2: """
-
-    if X.ndim! = 2:
+    if not isinstance(x, np.ndarray) or len(x.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
-    """ 
-    If n is less than 2, raise a ValueError
-    with the message X must contain multiple data points
-    """
 
-    if n < 2:
+    x = np.array(x)
+    if x.shape[0] < 2:
         raise ValueError("X must contain multiple data points")
-    """Calculate the mean for each dimension: """
-    mean = np.mean(X, axis=0, keepdims=True)
-
-    y = x - mean
-    n = x.shape[0]
-    cov = np.matmul(y.T, y) / (n - 1))
 
     return mean, cov
