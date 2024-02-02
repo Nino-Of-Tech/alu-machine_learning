b9e8a9ceda4d265809f3aa87acdf5fcc9e0684bf
Nino_Of_Tech <nwachukwunino15@gmail.com>
2024-02-02 23:46:34 +0200

    Machine Learning Regularization

--- supervised_learning/regularization/0-l2_reg_cost.py
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -5,6 +5,7 @@ cost of  a neural network with L2 regularization"""
 
 import numpy as np
 
+
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """returns cost of the network accounting
     for L2 regularization
