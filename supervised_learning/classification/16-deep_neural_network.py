b219c581cbcfa70eaf495376eca689c213500bbc
Nino_Of_Tech <nwachukwunino15@gmail.com>
2024-02-10 00:36:28 +0200

    Classification

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""This module is of a deep neural network
+perfomaing binary classification"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Deep Neural Network"""
+
+    def __init__(self, nx, layers):
+        """class constructor
+        nx - no. of input features
+        layers - list of nodes in each layer of the network"""
+
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        if (
+            type(layers) is not list
+            or len(layers) < 1
+            or min(layers) < 1
+        ):
+            raise TypeError("layers must be a list of positive integers")
+
+        # self.L - no. of layers in the neural network
+        self.L = len(layers)
+        # self.cache - dict of all intermediary values of the network
+        self.cache = {}
+
+        # weights - dict of all weights and biases of the network
+        self.weights = {}
+        for layer in range(self.L):
+            if layer == 0:
+                self.weights['W1'] = np.random.randn(
+                    layers[0], nx) * np.sqrt(2 / nx)
+                self.weights['b1'] = np.zeros([layers[0], 1])
+
+            else:
+                self.weights['W{}'.format(layer+1)] = np.random.randn(
+                    layers[layer],
+                    layers[layer-1]) * np.sqrt(2. / layers[layer-1])
+
+                self.weights['b{}'.format(
+                    layer+1)] = np.zeros((layers[layer], 1))
