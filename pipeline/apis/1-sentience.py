28b789d0b96518088b7aaed1f2016788330ab9b9
Nino-Of-Tech <nwachukwunino15@gmail.com>
2024-03-09 21:50:55 +0200

    Fixed some errors

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -6,6 +6,7 @@ uses Star Wars API to get the list of names of
 all sentinent species in the Star Wars universe"""
 
 import requests
+import time
 
 
 def sentientPlanets():
@@ -23,10 +24,16 @@ def sentientPlanets():
 
         url = data['next']
 
+        # Introduce a delay to avoid hitting API rate limits
+        time.sleep(0.5)  # You may adjust the delay as needed
+
     planets = []
     for species_name, homeworld_url in species:
         response = requests.get(homeworld_url)
         homeworld_data = response.json()
         planets.append(homeworld_data['name'])
+
+        # Introduce a delay to avoid hitting API rate limits
+        time.sleep(0.5)  # You may adjust the delay as needed
 
     return planets
