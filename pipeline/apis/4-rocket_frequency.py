149b8374c89ef6a1c29028cf46fcb1425e145f89
Nino-Of-Tech <nwachukwunino15@gmail.com>
2024-03-09 22:53:28 +0200

    Added new files

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -2,34 +2,39 @@
 
 """
 This module contains a function that
-uses Uonofficial SpaceX API to display
-no. of launches per rocket
+uses the unofficial SpaceX API to display
+the number of launches per rocket.
 """
 
 import requests
 
-
 def launches_per_rocket():
-    """print no. of launches per rocket"""
+    """Print the number of launches per rocket"""
     url = "https://api.spacexdata.com/v4/launches"
-    response = requests.get(url)
-    data = response.json()
     rockets = {}
-    for launch in data:
-        rocket_id = launch['rocket']
-        rocket_url = "https://api.spacexdata.com/v4/rockets/{}".format(
-            rocket_id)
-        # print(rocket_url)
-        rocket_response = requests.get(rocket_url)
-        rocket_data = rocket_response.json()
-        rocket_name = rocket_data['name']
-        if rocket_name in rockets:
-            rockets[rocket_name] += 1
-        else:
-            rockets[rocket_name] = 1
-    for rocket in sorted(rockets.items(), key=lambda x: (-x[1], x[0])):
-        print("{}: {}".format(rocket[0], rocket[1]))
 
+    # Retrieve all launches by handling pagination
+    while url:
+        response = requests.get(url)
+        data = response.json()
+
+        for launch in data:
+            rocket_id = launch['rocket']
+            rocket_url = f"https://api.spacexdata.com/v4/rockets/{rocket_id}"
+            rocket_response = requests.get(rocket_url)
+            rocket_data = rocket_response.json()
+            rocket_name = rocket_data['name']
+            if rocket_name in rockets:
+                rockets[rocket_name] += 1
+            else:
+                rockets[rocket_name] = 1
+
+        # Check if there are more pages
+        url = response.links.get('next', {}).get('url')
+
+    # Sort and print the results
+    for rocket in sorted(rockets.items(), key=lambda x: (-x[1], x[0])):
+        print(f"{rocket[0]}: {rocket[1]}")
 
 if __name__ == "__main__":
     launches_per_rocket()
