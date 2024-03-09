8ecc8909a5abb4519b8a5effc9471b3821db83b9
Nino-Of-Tech <nwachukwunino15@gmail.com>
2024-03-09 21:57:18 +0200

    Fixed some errors

--- pipeline/apis/1-sentience.py
+++ pipeline/apis/1-sentience.py
@@ -6,36 +6,15 @@ uses Star Wars API to get the list of names of
 all sentinent species in the Star Wars universe"""
 
 import requests
-import time
 
 
 def sentientPlanets():
-    """Return list of names of home planets of all sentient species"""
+    """return list of names of all sentient species"""
     url = "https://swapi-api.alx-tools.com/api/species/"
+    response = requests.get(url)
+    data = response.json()
     species = []
-
-    while url:
-        response = requests.get(url)
-        data = response.json()
-
-        print("Received data:", data)  # Print received data for debugging
-
-        for result in data.get('results', []):
-            if result.get('designation') == "sentient":
-                species.append((result.get('name'), result.get('homeworld')))
-
-        url = data.get('next')
-
-        # Introduce a delay to avoid hitting API rate limits
-        time.sleep(0.5)
-
-    planets = []
-    for species_name, homeworld_url in species:
-        response = requests.get(homeworld_url)
-        homeworld_data = response.json()
-        planets.append(homeworld_data.get('name'))
-
-        # Introduce a delay to avoid hitting API rate limits
-        time.sleep(0.5)
-
-    return planets
+    for result in data['results']:
+        if result['designation'] == "sentient":
+            species.append(result['name'])
+    return species
