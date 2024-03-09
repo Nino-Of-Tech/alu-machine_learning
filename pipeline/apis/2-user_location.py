f692b887cfbeb917c14ac269184748787281ac42
Nino-Of-Tech <nwachukwunino15@gmail.com>
2024-03-09 22:12:28 +0200

    Fixed some errors

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
-""" Script for getting user location"""
-import sys
+
+""" Print location of specific users"""
+
 import requests
-import time
+import sys
+
+# get the url from string passed on the terminal
 
 
-if __name__ == '__main__':
+def print_location():
+    """print location of user"""
     url = sys.argv[1]
     response = requests.get(url)
-    res = response.json()
+    data = response.json()
 
     if response.status_code == 200:
-        print(res['location'])
-    elif response.status_code == 404:
-        print('Not found')
+        if 'location' in data and data['location']:
+            print(data['location'])
+        else:
+            print("Not found")
     elif response.status_code == 403:
-        limit = int(response.headers['X-Ratelimit-Reset'])
-        start = int(time.time())
-        elapsed = int((limit - start) / 60)
-        print('Reset in {} min'.format(int(elapsed)))
+        print("Reset in {} min".format(
+            (int(response.headers['X-Ratelimit-Reset']) -
+             int(response.headers['X-Ratelimit-Reset'])) / 60))
+    else:
+        print("Not found")
+
+
+if __name__ == "__main__":
+    print_location()
