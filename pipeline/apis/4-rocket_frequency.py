2ae8dbf62d124dcd1d08f25e9d815401c0aaec63
Nino-Of-Tech <nwachukwunino15@gmail.com>
2024-03-09 23:02:31 +0200

    Added new files

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -8,33 +8,40 @@ the number of launches per rocket.
 
 import requests
 
-def launches_per_rocket():
-    """Print the number of launches per rocket"""
+def get_all_launches():
+    """
+    Retrieve all launches from the SpaceX API.
+    """
     url = "https://api.spacexdata.com/v4/launches"
-    rockets = {}
+    all_launches = []
 
     # Retrieve all launches by handling pagination
     while url:
         response = requests.get(url)
         data = response.json()
-
-        for launch in data:
-            rocket_id = launch['rocket']
-            rocket_url = f"https://api.spacexdata.com/v4/rockets/{rocket_id}"
-            rocket_response = requests.get(rocket_url)
-            rocket_data = rocket_response.json()
-            rocket_name = rocket_data['name']
-            if rocket_name in rockets:
-                rockets[rocket_name] += 1
-            else:
-                rockets[rocket_name] = 1
-
-        # Check if there are more pages
+        all_launches.extend(data)
         url = response.links.get('next', {}).get('url')
 
+    return all_launches
+
+def launches_per_rocket():
+    """
+    Print the number of launches per rocket.
+    """
+    all_launches = get_all_launches()
+    rockets = {}
+
+    for launch in all_launches:
+        rocket_id = launch['rocket']
+        rocket_name = launch['rocket']['name']
+        if rocket_name in rockets:
+            rockets[rocket_name] += 1
+        else:
+            rockets[rocket_name] = 1
+
     # Sort and print the results
-    for rocket in sorted(rockets.items(), key=lambda x: (-x[1], x[0])):
-        print(f"{rocket[0]}: {rocket[1]}")
+    for rocket, count in sorted(rockets.items(), key=lambda x: (-x[1], x[0])):
+        print(f"{rocket}: {count}")
 
 if __name__ == "__main__":
     launches_per_rocket()
