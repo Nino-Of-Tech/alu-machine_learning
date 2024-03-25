86399396d32e815f55cdce97b0ad3429417dc481
Nino-Of-Tech <nwachukwunino15@gmail.com>
2024-03-25 18:16:30 +0200

    Added the python scripts

---
+++ pipeline/databases/31-insert_school.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+function that inserts a new document
+in a collection based on keyword args
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Insert new collection
+    return new _id
+    mongo_collection - pymongo collection object
+    """
+    document = mongo_collection.insert_one(kwargs)
+    return (document.inserted_id)
